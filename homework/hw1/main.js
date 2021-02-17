5655dc694666cc0d918c8d867d42597652510ab8
LeonKazaryan <leon.aksim@gmail.com>
2021-02-17 14:17:00 +0600

    the second chapter

---
+++ homework/hw1/main.js
@@ -0,0 +1,33 @@
+let number = 0;
+console.log("В коробке: " + number +" яблок.");
+
+if (number > 0){
+    console.log("В коробке больше нуля яблок");
+} else if (number == 0) {
+    console.log("В коробке пусто");
+} else{
+    console.log("У вас должок перед коробко! Как так вообще?")
+} 
+
+number += 500;
+console.log("В коробке: " + number +" яблок.");
+
+if (number > 0){
+    console.log("В коробке положительное значение яблок.");
+} else if (number == 0) {
+    console.log("В коробке пусто");
+} else{
+    console.log("У вас должок перед коробко! Как так вообще?")
+} 
+
+number -= 1000;
+console.log("В коробке: " + number +" яблок.");
+
+
+if (number > 0){
+    console.log("В коробке больше нуля яблок");
+} else if (number== 0) {
+    console.log("В коробке пусто");
+} else{
+    console.log("У вас должок перед коробко! Как так вообще?")
+} 
