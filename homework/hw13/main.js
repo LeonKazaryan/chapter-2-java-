e90dea58ad82312bb6764ca33f9b784b974014e9
LeonKazaryan <leon.aksim@gmail.com>
2021-03-25 19:09:25 +0600

    кк

---
+++ homework/hw13/main.js
@@ -0,0 +1,29 @@
+console.log("-------------------------")
+
+function sumofnumbers (num) {
+    let tmp = "" + num
+    let sum = 0
+    for (let i = 0; i < tmp.length; i++) 
+      sum += + tmp[i]
+  
+    return sum
+  }
+
+console.log("Сумма чисел данного числа является: " + sumofnumbers(228))
+
+console.log("-------------------------")
+
+function isPalindrome(str) {
+    let strReverse = str.split('').reverse().join(''); 
+    if (strReverse === str) {
+      return 'Ух-ты, ваше слово "' + str + '" является палиндромом! ^-^ ' ;
+    } else {
+      console.log("Ваше слово: " + str);
+      console.log("Это же слово наоборот: " + strReverse)
+      return "Увы, но это не палиндром. :(";
+    }
+  }
+
+console.log(isPalindrome("шавыавыаыввылаш"))
+
+console.log("-------------------------")
