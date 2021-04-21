6a8a33f382bbb03d4d11f4c89ffcc3308a0d2f69
LeonKazaryan <leon.aksim@gmail.com>
2021-04-21 19:39:45 +0600

    hij

---
+++ abstract/abstract-6/main.js
@@ -0,0 +1,190 @@
+// Promise - обещание
+
+const sleep = (delay) => new Promise((resolve) => {
+  setTimeout(() => {
+    
+    console.log("Подождали " + delay);
+    resolve();
+  }, delay);
+});
+
+
+window.addEventListener("load", async () => {
+  let start;
+  
+  console.log("Начали");
+
+  start = Date.now();
+
+  await sleep(500);
+  await sleep(1000);
+  
+  console.log("Закончили " + (Date.now() - start));
+
+
+  console.log("Начали");
+
+  start = Date.now();
+  await Promise.all([
+    sleep(500),
+    sleep(1000)
+  ]); // все
+  // Promise.race //гонка
+  
+  console.log("Закончили " + (Date.now() - start));
+
+  
+  console.log("Начали");
+
+  start = Date.now();
+  await Promise.race([
+    sleep(500),
+    sleep(1000)
+  ]); 
+  
+  console.log("Закончили " + (Date.now() - start));
+
+});
+
+
+
+
+
+
+
+// const getA = async () => new Promise((resolve, reject) => {
+//   reject("Не нашли двойку");
+//   // resolve(2);
+//   setTimeout(() => resolve(2), 200);
+// });
+
+// window.addEventListener("load", async () => {
+
+//   try {
+//     let value = await getA();
+
+//     // разрыв веременного континуума 
+//     // callback
+  
+//     let value2 = await getA();
+    
+//     // разрыв веременного континуума 
+//     // callback
+//     console.log(value, value2);
+//   } catch (e) {
+//     console.log(e);
+//     let value3 = await getA(); 
+//   } finally {
+//     console.log("Ну и ладно");
+//     // console.log(value);
+//   }
+//   // console.log("Ну и ладно");
+
+
+//   // let a = getA()
+//   //   .then((value) => {
+//   //     console.log(value);
+//   //     return getA();
+//   //   })
+//   //   .then(value => {
+//   //     console.log(value);
+//   //   })
+//   //   .catch(reason => {
+//   //     console.log(reason);
+//   //     return 3;
+//   //   })
+//   //   .then(value => {
+//   //     console.log("Но нашли тройку");
+//   //   });
+  
+  
+// });
+
+
+
+
+
+// const add = x => y => callback => {
+//   setTimeout(() => callback(x + y), 0);
+// }
+
+
+// {
+//   let x = 10;
+//   let y = 20;
+
+//   add (x) (y) ((sum) => {
+//     add (sum) (2) ((sum2) => {
+//       console.log(sum, sum2);
+//     });
+//   });
+
+// }
+
+
+// const asyncAction = (callback) => {
+//   // действия
+//   console.log("actionStared");
+  
+//   setTimeout(() => {
+//     console.log("action");
+//     callback();
+//   });
+// }
+
+
+// // callback hell
+// {
+//   let a = 1;
+
+//   asyncAction(() => {
+//     console.log("done");
+
+//     asyncAction(() => {
+//       console.log("done");
+      
+//       asyncAction(() => {
+//         console.log("done");
+//       });
+
+//     });
+
+//   });
+  
+  
+
+//   console.log("started");
+// }
+
+
+// {
+//   let a = 1;
+//   console.log("1", a);
+
+//   setTimeout(() => {
+//     a += 1;
+//     console.log("2", a) // 2
+
+//     setTimeout(() => {
+//       a += 1;
+//       console.log("3", a) // 4
+//     }, 100); 
+  
+//     console.log("2 end", a) // 2
+
+//   }, 100);
+
+//   setTimeout(() => {
+//     a += 1;
+//     console.log("4", a); // 3
+//   }, 100); 
+
+//   console.log("1 end", a); // 1
+
+//   // Дождаться ответа от сервера
+
+//   let i = 0;
+//   while (i++ < 100) {
+//     console.log("loop");
+//   }
+// }
