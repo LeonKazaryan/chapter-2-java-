b85f6060de34844de3b42bada541d3f1f4a8d416
LeonKazaryan <leon.aksim@gmail.com>
2021-04-22 19:40:26 +0600

    hw canvas

---
+++ exercises/ex7/canvas.js
@@ -0,0 +1,360 @@
+const draw = (canvas) => {
+
+  /**
+   * @type {CanvasRenderingContext2D}
+   */
+  const context = canvas.getContext("2d"); // webgl webgl2
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-30,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-120  ,-30);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+
+  context.translate(-120  ,-30);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+
+  context.translate(-120  ,-30);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+
+  context.translate(-120  ,-30);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+
+  context.translate(-120  ,-30);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-220,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-220,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+  context.translate(-220,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+
+
+  context.translate(-220,-120);
+
+  context.beginPath();
+  context.fillStyle="black";
+  context.fillRect(100,100,100,100);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(80,80,80,80);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(60,60,60,60);
+  context.translate(30,30);
+  context.fillStyle="white";
+  context.fillRect(40,40,40,40);
+  context.translate(30,30);
+  context.fillStyle="black";
+  context.fillRect(20,20,20,20);
+  context.closePath();
+  console.log(context);
+}
--- homework/hw(canvas)/canvas.js
+++ homework/hw(canvas)/canvas.js
@@ -4,43 +4,41 @@ const draw = (canvas) => {
    * @type {CanvasRenderingContext2D}
    */
   const context = canvas.getContext("2d"); // webgl webgl2
-  let Pi = Math.PI*25;
-  function CreateTriangle(){
-    context.beginPath();
-    context.moveTo(25,0);
-    context.lineTo(0,25);
-    context.lineTo(50,25);
-    context.fill();
-    context.closePath();
-  }
-  let y = 25;
-  function CreateSemicircle(){
-    context.beginPath();
-    CreateTriangle();
-    context.translate(Pi,y);
-    CreateTriangle();
-    context.translate(Pi/4,2*y);
-    CreateTriangle();
-    context.translate(-Pi/4,2*y);
-    CreateTriangle();
-    context.translate(-Pi,y);
-    CreateTriangle();
-    context.closePath();
-  }
-  context.translate(700,200);
-  CreateSemicircle();
-  context.scale(-1,1);
-  context.translate(-(2*y),-(6*y));
-  CreateSemicircle()
-
-
-
   
+  function CreatreCircle(y){
+    let Pi = Math.PI*y;
 
-  
+    function CreateTriangle(){
+      context.beginPath();
+      context.moveTo(y,0);
+      context.lineTo(0,y);
+      context.lineTo(2*y,y);
+      context.fill();
+      context.closePath();
+    }
 
+    function CreateSemicircle(){
+      context.beginPath();
+      CreateTriangle();
+      context.translate(Pi,y);
+      CreateTriangle();
+      context.translate(Pi/4,2*y);
+      CreateTriangle();
+      context.translate(-Pi/4,2*y);
+      CreateTriangle();
+      context.translate(-Pi,y);
+      CreateTriangle();
+      context.closePath();
+    }
+    context.translate(490,130);
+    CreateSemicircle();
+    context.scale(-1,1);
+    context.translate(-(2*y),-(6*y));
+    CreateSemicircle();
 
+  }
 
-  console.log(context);
+//ниже вызываем функцию по созданию круга и передаем значение размера треугольника
 
+  CreatreCircle(25);
 }
